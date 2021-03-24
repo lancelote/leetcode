77f6138f3ae7e7b4751c90dec613a2d4e1e65f94
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-03-24 15:51:41 +0300

    Richest customer

---
+++ tests/test_richest_customer_wealth.py
@@ -0,0 +1,15 @@
+import pytest
+
+from src.richest_customer_wealth import Solution
+
+
+@pytest.mark.parametrize(
+    "data,richest",
+    [
+        ([[1, 2, 3], [3, 2, 1]], 6),
+        ([[1, 5], [7, 3], [3, 5]], 10),
+        ([[2, 8, 7], [7, 1, 3], [1, 9, 5]], 17),
+    ],
+)
+def test_solution(data, richest):
+    assert Solution().maximumWealth(data) == richest
