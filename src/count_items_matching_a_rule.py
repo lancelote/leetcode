d9a0d83e8a212ec5d8326e25a14665d0a844a4ce
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-04-03 14:53:29 +0300

    Count items

---
+++ src/count_items_matching_a_rule.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def countMatches(
+        self, items: List[List[str]], rule_key: str, rule_value: str
+    ) -> int:
+        count = 0
+
+        for item_type, item_color, item_name in items:
+            if rule_key == "type" and item_type == rule_value:
+                count += 1
+            elif rule_key == "color" and item_color == rule_value:
+                count += 1
+            elif rule_key == "name" and item_name == rule_value:
+                count += 1
+
+        return count
---
+++ tests/test_count_items_matching_a_rule.py
@@ -0,0 +1,32 @@
+import pytest
+
+from src.count_items_matching_a_rule import Solution
+
+
+@pytest.mark.parametrize(
+    "items,rule_key,rule_value,matching",
+    [
+        (
+            [
+                ["phone", "blue", "pixel"],
+                ["computer", "silver", "lenovo"],
+                ["phone", "gold", "iphone"],
+            ],
+            "color",
+            "silver",
+            1,
+        ),
+        (
+            [
+                ["phone", "blue", "pixel"],
+                ["computer", "silver", "phone"],
+                ["phone", "gold", "iphone"],
+            ],
+            "type",
+            "phone",
+            2,
+        ),
+    ],
+)
+def test_solution(items, rule_key, rule_value, matching):
+    assert Solution().countMatches(items, rule_key, rule_value) == matching
