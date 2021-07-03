ccf93e6cb4bd520177d3e39ff7efed7ee5931910
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-07-03 19:28:55 +0300

    Cyclically rotating a grid

---
+++ problems/src/cyclically_rotating_a_grid.py
@@ -0,0 +1,63 @@
+from typing import Iterator
+from typing import Tuple
+
+Grid = list[list[int]]
+Layer = list[int]
+Layers = list[Layer]
+Coordinates = Tuple[int, int]
+
+
+def layer_coordinates(
+    layer_id: int, width: int, height: int
+) -> Iterator[Coordinates]:
+    x = layer_id
+    y = layer_id
+
+    for x in range(layer_id, width - layer_id):
+        yield x, y
+
+    for y in range(layer_id + 1, height - layer_id):
+        yield x, y
+
+    for x in range(width - layer_id - 2, layer_id - 1, -1):
+        yield x, y
+
+    for y in range(height - layer_id - 2, layer_id, -1):
+        yield x, y
+
+
+def layers_from(grid: Grid) -> Iterator[Layer]:
+    grid_width = len(grid[0])
+    grid_height = len(grid)
+    total_layers = min(grid_width, grid_height) // 2
+
+    for layer_id in range(total_layers):
+        layer = [
+            grid[y][x]
+            for x, y in layer_coordinates(layer_id, grid_width, grid_height)
+        ]
+        yield layer
+
+
+def rotate(layer: Layer, by: int) -> Iterator[int]:
+    shift = by % len(layer)
+
+    for i in range(shift, len(layer)):
+        yield layer[i]
+
+    for i in range(shift):
+        yield layer[i]
+
+
+class Solution:
+    def rotateGrid(self, grid: Grid, k: int) -> Grid:
+        grid_width = len(grid[0])
+        grid_height = len(grid)
+
+        for layer_id, layer in enumerate(layers_from(grid)):
+            for (x, y), item in zip(
+                layer_coordinates(layer_id, grid_width, grid_height),
+                rotate(layer, by=k),
+            ):
+                grid[y][x] = item
+        return grid
---
+++ problems/tests/test_cyclically_rotating_a_grid.py
@@ -0,0 +1,66 @@
+import pytest
+
+from problems.src.cyclically_rotating_a_grid import layer_coordinates
+from problems.src.cyclically_rotating_a_grid import Solution
+
+
+@pytest.mark.parametrize(
+    "layer_id,width,height,coordinates",
+    [
+        (
+            0,
+            4,
+            6,
+            [
+                (0, 0),
+                (1, 0),
+                (2, 0),
+                (3, 0),
+                (3, 1),
+                (3, 2),
+                (3, 3),
+                (3, 4),
+                (3, 5),
+                (2, 5),
+                (1, 5),
+                (0, 5),
+                (0, 4),
+                (0, 3),
+                (0, 2),
+                (0, 1),
+            ],
+        ),
+        (
+            1,
+            4,
+            6,
+            [
+                (1, 1),
+                (2, 1),
+                (2, 2),
+                (2, 3),
+                (2, 4),
+                (1, 4),
+                (1, 3),
+                (1, 2),
+            ],
+        ),
+    ],
+)
+def test_layer_coordinates(layer_id, width, height, coordinates):
+    assert list(layer_coordinates(layer_id, width, height)) == coordinates
+
+
+@pytest.mark.parametrize(
+    "grid,k,expected",
+    [
+        ([[40, 10], [30, 20]], 1, [[10, 20], [40, 30]]),
+        (
+            [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]],
+            2,
+            [[3, 4, 8, 12], [2, 11, 10, 16], [1, 7, 6, 15], [5, 9, 13, 14]],
+        ),
+    ],
+)
+def test_solution(grid, k, expected):
+    assert Solution().rotateGrid(grid, k) == expected
