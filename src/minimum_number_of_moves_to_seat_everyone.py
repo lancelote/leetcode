d3c3be5631148d984cbd6d27bf1349c9eb2dd55d
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-11-08 20:27:38 +0000

    Fix "Minimum Number of Moves to Seat Everyone" solution

--- src/minimum_number_of_moves_to_seat_everyone.py
+++ src/minimum_number_of_moves_to_seat_everyone.py
@@ -1,3 +1,9 @@
 class Solution:
     def minMovesToSeat(self, seats: list[int], students: list[int]) -> int:
-        return abs(sum(seats) - sum(students))
+        sorted_seats = sorted(seats)
+        sorted_students = sorted(students)
+
+        return sum(
+            abs(seat - student)
+            for (seat, student) in zip(sorted_seats, sorted_students)
+        )
