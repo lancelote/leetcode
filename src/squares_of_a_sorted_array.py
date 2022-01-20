bb9002980a4a1982a8bfc0147af740cbc6ca6727
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-01-20 20:48:03 +0300

    Simplify "Squares of a Sorted Array" solution

--- src/squares_of_a_sorted_array.py
+++ src/squares_of_a_sorted_array.py
@@ -1,36 +1,18 @@
-from typing import List
-
-
 class Solution:
-    @staticmethod
-    def get_non_negative_index(array: List[int]) -> int:
-        for i, item in enumerate(array):
-            if item >= 0:
-                return i
-        return len(array)
-
-    def sortedSquares(self, array: List[int]) -> List[int]:
+    def sortedSquares(self, nums: list[int]) -> list[int]:
+        left_index = 0
+        right_index = len(nums) - 1
         result = []
-        non_negative_index = self.get_non_negative_index(array)
-        negative_index = non_negative_index - 1
 
-        while negative_index >= 0 and non_negative_index < len(array):
-            negative_item = array[negative_index]
-            non_negative_item = array[non_negative_index]
+        while left_index <= right_index:
+            left_value = nums[left_index]
+            right_value = nums[right_index]
 
-            if non_negative_item > abs(negative_item):
-                result.append(negative_item ** 2)
-                negative_index -= 1
+            if abs(left_value) > abs(right_value):
+                left_index += 1
+                result.append(left_value * left_value)
             else:
-                result.append(non_negative_item ** 2)
-                non_negative_index += 1
+                right_index -= 1
+                result.append(right_value * right_value)
 
-        while negative_index >= 0:
-            result.append(array[negative_index] ** 2)
-            negative_index -= 1
-
-        while non_negative_index < len(array):
-            result.append(array[non_negative_index] ** 2)
-            non_negative_index += 1
-
-        return result
+        return result[::-1]
