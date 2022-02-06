947775854a597ad40fb27f830f863f167530d448
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-02-06 15:22:01 +0300

    Remove wild print

--- src/xor_operation_in_an_array.py
+++ src/xor_operation_in_an_array.py
@@ -2,6 +2,5 @@ class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         result = 0
         for i in range(start, n * 2 + start, 2):
-            print(i)
             result ^= i
         return result
