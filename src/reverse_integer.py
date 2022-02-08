086e918469a2206596be71def0606798d5680968
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-02-08 19:32:30 +0300

    Fix style

--- src/reverse_integer.py
+++ src/reverse_integer.py
@@ -2,7 +2,7 @@ class Solution:
     def reverse(self, x: int) -> int:
         result = int(str(abs(x))[::-1])
 
-        if -(2 ** 31) - 1 < result < 2 ** 31:
+        if -(2**31) - 1 < result < 2**31:
             return -result if x < 0 else result
 
         return 0
