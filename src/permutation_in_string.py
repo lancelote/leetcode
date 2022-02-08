98e18404d5a952b78e92a5767a8595e3b3fa0b26
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-02-08 21:23:25 +0300

    Faster solution for "Permutation in String"

--- src/permutation_in_string.py
+++ src/permutation_in_string.py
@@ -1,31 +1,42 @@
+def to_int(char: str) -> int:
+    return ord(char) - ord("a")
+
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
 
-        s1_counter: dict[str, int] = {}
-        s2_counter: dict[str, int] = {}
+        s1_counter = [0] * 26
+        s2_counter = [0] * 26
 
-        for i in range(len(s1)):
-            x1 = s1[i]
-            x2 = s2[i]
+        for x1, x2 in zip(s1, s2):
+            s1_counter[to_int(x1)] += 1
+            s2_counter[to_int(x2)] += 1
 
-            s1_counter[x1] = 1 + s1_counter.get(x1, 0)
-            s2_counter[x2] = 1 + s2_counter.get(x2, 0)
+        similar = sum(c1 == c2 for c1, c2 in zip(s1_counter, s2_counter))
 
-        if s1_counter == s2_counter:
-            return True
-
-        for i in range(len(s1), len(s2)):
-            to_remove = s2[i - len(s1)]
-            to_add = s2[i]
-
-            s2_counter[to_remove] -= 1
-            if s2_counter[to_remove] == 0:
-                del s2_counter[to_remove]
-            s2_counter[to_add] = 1 + s2_counter.get(to_add, 0)
-
-            if s1_counter == s2_counter:
+        left = 0
+        for right in range(len(s1), len(s2)):
+            if similar == 26:
                 return True
 
-        return False
+            new = to_int(s2[right])
+            s2_counter[new] += 1
+
+            if s1_counter[new] == s2_counter[new]:
+                similar += 1
+            elif s1_counter[new] + 1 == s2_counter[new]:
+                similar -= 1
+
+            old = to_int(s2[left])
+            s2_counter[old] -= 1
+
+            if s1_counter[old] == s2_counter[old]:
+                similar += 1
+            elif s1_counter[old] - 1 == s2_counter[old]:
+                similar -= 1
+
+            left += 1
+
+        return similar == 26
