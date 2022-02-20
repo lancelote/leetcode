a38c2c2601c8912ef8b7da3870aa215fb9cd343b
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-02-20 20:52:02 +0300

    Simplify "Flood Fill"

--- src/flood_fill.py
+++ src/flood_fill.py
@@ -1,57 +1,46 @@
 from collections import deque
 from typing import Deque
-from typing import Iterator
-from typing import NamedTuple
 
 Image = list[list[int]]
 
 
-class Pixel(NamedTuple):
-    x: int
-    y: int
-    value: int
-
-
 SHIFTS = [
-    (0, +1),
     (+1, 0),
-    (0, -1),
+    (0, +1),
     (-1, 0),
+    (0, -1),
 ]
 
 
-def neighbors(pixel: Pixel, image: Image) -> Iterator[Pixel]:
-    for dx, dy in SHIFTS:
-        new_x = pixel.x + dx
-        new_y = pixel.y + dy
-
-        valid_x = 0 <= new_x < len(image[0])
-        valid_y = 0 <= new_y < len(image)
-
-        if valid_x and valid_y:
-            yield Pixel(new_x, new_y, value=image[new_y][new_x])
-
-
 class Solution:
-    def floodFill(self, image: Image, y: int, x: int, new_color: int) -> Image:
+    def floodFill(
+        self, image: Image, row: int, col: int, new_color: int
+    ) -> Image:
         assert image
         assert image[0]
 
-        start_pixel = Pixel(x, y, value=image[y][x])
-        base_color = start_pixel.value
+        rows = len(image)
+        cols = len(image[0])
+        start_pixel = (row, col)
+        base_color = image[row][col]
 
         if new_color == base_color:
             return image
 
-        to_paint: Deque[Pixel] = deque()
+        to_paint: Deque[tuple[int, int]] = deque()
         to_paint.append(start_pixel)
 
         while to_paint:
-            pixel = to_paint.popleft()
-            image[pixel.y][pixel.x] = new_color
+            row, col = to_paint.popleft()
+            valid_row = 0 <= row < rows
+            valid_col = 0 <= col < cols
 
-            for neighbor in neighbors(pixel, image):
-                if neighbor.value == base_color:
-                    to_paint.append(neighbor)
+            if not (valid_row and valid_col) or image[row][col] != base_color:
+                continue
+
+            image[row][col] = new_color
+
+            for d_row, d_col in SHIFTS:
+                to_paint.append((row + d_row, col + d_col))
 
         return image
