1fa47a371ad6279fe86a1c8012fed7451843f860
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-04-02 00:40:50 +0300

    "Binary Search" solution simplified

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,17 +1,15 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        left_index = 0
-        right_index = len(nums) - 1
+        left, right = 0, len(nums) - 1
 
-        while left_index <= right_index:
-            middle_index = left_index + (right_index - left_index) // 2
-            middle_value = nums[middle_index]
+        while left <= right:
+            middle = left + (right - left) // 2
 
-            if middle_value == target:
-                return middle_index
-            elif middle_value > target:
-                right_index = middle_index - 1
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right = middle - 1
             else:
-                left_index = middle_index + 1
+                left = middle + 1
 
         return -1
--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -12,6 +12,7 @@ from src.binary_search import Solution
         ([1, 2, 3], 3, 2),
         ([1, 2, 3, 4], 1, 0),
         ([1, 2, 3, 4], 4, 3),
+        ([-1, 0, 3, 5, 9, 12], 13, -1),
     ],
 )
 def test_solution(nums, target, expected):
