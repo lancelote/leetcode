83a59699918372a4c0e925dd7be50f52379d3f8b
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-06-13 22:55:13 +0200

    Fix formatting

--- tests/test_product_of_array_except_self.py
+++ tests/test_product_of_array_except_self.py
@@ -8,7 +8,7 @@ from src.product_of_array_except_self import Solution
     [
         ([1, 2, 3, 4], [24, 12, 8, 6]),
         ([-1, 1, 0, -3, 3], [0, 0, 9, 0, 0]),
-    ]
+    ],
 )
 def test_solution(nums, expected):
     assert Solution().productExceptSelf(nums) == expected
