0ec8c9ae7d480840034f448a91d7d33b3f3615ea
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-08-02 23:34:30 +0200

    Remove duplicate parens storage

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -5,7 +5,7 @@ class Solution:
     def isValid(self, s: str) -> bool:
         expected_stack: list[str] = []
         for paren in s:
-            if paren in {"(", "{", "["}:
+            if paren in TO_CLOSING:
                 expected_stack.append(TO_CLOSING[paren])
             elif not expected_stack or expected_stack.pop() != paren:
                 return False
