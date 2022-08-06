bef7a92bba72bf9a3e8154cd455c059eb21babc9
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-08-06 23:43:28 +0200

    Add missing constructor return type

--- src/merge_two_sorted_lists.py
+++ src/merge_two_sorted_lists.py
@@ -2,7 +2,9 @@ from typing import Optional
 
 
 class ListNode:
-    def __init__(self, val: int = 0, next: Optional["ListNode"] = None):
+    def __init__(
+        self, val: int = 0, next: Optional["ListNode"] = None
+    ) -> None:
         self.val = val
         self.next = next
 
