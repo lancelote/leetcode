a6d8240ac65ba4a42b067fecccea23f3bcfa4a18
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-08-07 14:55:23 +0200

    Rename `head` to `tail`

--- src/merge_two_sorted_lists.py
+++ src/merge_two_sorted_lists.py
@@ -6,26 +6,26 @@ class Solution:
         self, l1: ListNode | None, l2: ListNode | None
     ) -> ListNode | None:
         dummy = ListNode()
-        head = dummy
+        tail = dummy
 
         while l1 and l2:
             if l1.val > l2.val:
-                head.next = l2
-                head = l2
+                tail.next = l2
+                tail = l2
                 l2 = l2.next
             else:
-                head.next = l1
-                head = l1
+                tail.next = l1
+                tail = l1
                 l1 = l1.next
 
         while l1:
-            head.next = l1
-            head = l1
+            tail.next = l1
+            tail = l1
             l1 = l1.next
 
         while l2:
-            head.next = l2
-            head = l2
+            tail.next = l2
+            tail = l2
             l2 = l2.next
 
         return dummy.next
