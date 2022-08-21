11b92a3cb0942737fd3818cf083676fe9c960c32
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-08-21 12:50:05 +0200

    "Stamping The Sequence" solution

--- src/stamping_the_sequence.py
+++ src/stamping_the_sequence.py
@@ -1,28 +1,36 @@
+def is_valid(subseq: list[str], stamp: str) -> bool:
+    result = False
+    for i in range(len(subseq)):
+        if subseq[i] == "?":
+            continue
+        elif subseq[i] != stamp[i]:
+            return False
+        elif subseq[i] == stamp[i]:
+            result = True
+    return result
+
+
 class Solution:
     def movesToStamp(self, stamp: str, target: str) -> list[int]:
         result = []
-        turns = 0
-        done = [False] * len(target)
         todo = len(target)
+        target_list = list(target)
 
-        while turns < 10 * len(target) and todo != 0:
+        while todo:
+            proceed = False
+
             for i in range(len(target) - len(stamp) + 1):
-                possible_stamp = False
-
-                for j in range(len(stamp)):
-                    if not done[i + j]:
-                        possible_stamp = True
-                    if target[i + j] != stamp[j] and not done[i + j]:
-                        possible_stamp = False
-                        break
-
-                if possible_stamp:
-                    for j in range(len(stamp)):
-                        if not done[i + j]:
-                            done[i + j] = True
-                            todo -= 1
+                if is_valid(target_list[i : i + len(stamp)], stamp):
+                    proceed = True
                     result.append(i)
-                    break
-            turns += 1
 
-        return result[::-1] if turns < 10 * len(target) else []
+                    for j in range(len(stamp)):
+                        if target_list[i + j] != "?":
+                            target_list[i + j] = "?"
+                            todo -= 1
+                    break
+
+            if not proceed:
+                return []
+
+        return result[::-1]
