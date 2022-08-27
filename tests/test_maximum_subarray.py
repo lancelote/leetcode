b7547d0bb500bbbc5cd80387a53791845f9104a9
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-08-27 14:21:45 +0200

    Extra test for maximum subarray
    With all items negative

--- tests/test_maximum_subarray.py
+++ tests/test_maximum_subarray.py
@@ -9,6 +9,7 @@ from src.maximum_subarray import Solution
         ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
         ([1], 1),
         ([5, 4, -1, 7, 8], 23),
+        ([-3, -2, -1], -1),
     ],
 )
 def test_solution(nums, expected_sub):
