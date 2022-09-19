6f9549defd9e49e82b4464a55466c7071f7bd052
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-09-19 21:27:53 +0200

    "Trapping Rain Water" O(1) memory

--- src/trapping_rain_water.py
+++ src/trapping_rain_water.py
@@ -1,22 +1,18 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        n = len(height)
+        result = 0
 
-        current_max_left = 0
-        max_left = []
+        max_left, max_right = 0, 0
+        left, right = 0, len(height) - 1
 
-        for x in height:
-            max_left.append(current_max_left)
-            current_max_left = max(current_max_left, x)
-
-        current_max_right = 0
-        max_right = [0] * n
-
-        for i in range(n - 1, -1, -1):
-            max_right[i] = current_max_right
-            current_max_right = max(current_max_right, height[i])
-
-        min_border = [min(x, y) for x, y in zip(max_left, max_right)]
-        result = sum(x - y for x, y in zip(min_border, height) if x - y > 0)
+        while left != right:
+            if height[left] <= height[right]:
+                result += max(0, max_left - height[left])
+                max_left = max(max_left, height[left])
+                left += 1
+            else:
+                result += max(0, max_right - height[right])
+                max_right = max(max_right, height[right])
+                right -= 1
 
         return result
