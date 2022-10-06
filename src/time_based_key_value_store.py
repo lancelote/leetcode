5e7c89424130479acc8a8a01a5b60e59ff865f8a
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-10-06 23:06:33 +0200

    "Time Based Key-Value Store" solution

---
+++ src/time_based_key_value_store.py
@@ -0,0 +1,16 @@
+from bisect import bisect_right
+from collections import defaultdict
+
+
+class TimeMap:
+    def __init__(self):
+        self.data: dict[str, list[tuple[int, str]]] = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.data[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        i = bisect_right(self.data[key], timestamp, key=lambda x: x[0])
+        if i > 0:
+            return self.data[key][i - 1][1]
+        return ""
---
+++ tests/test_time_based_key_value_store.py
@@ -0,0 +1,11 @@
+from src.time_based_key_value_store import TimeMap
+
+
+def test_solution():
+    tm = TimeMap()
+    tm.set("foo", "bar", 1)
+    assert tm.get("foo", 1) == "bar"
+    assert tm.get("foo", 3) == "bar"
+    tm.set("foo", "bar2", 4)
+    assert tm.get("foo", 4) == "bar2"
+    assert tm.get("foo", 5) == "bar2"
