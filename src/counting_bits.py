9ed791f3894cba56a401487f2eb087d4ede82907
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-10-29 16:19:50 +0200

    "Counting Bits" solution

--- src/counting_bits.py
+++ src/counting_bits.py
@@ -1,14 +1,11 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
-        result: list[int] = []
+        dp = [0] * (n + 1)
+        offset = 1
 
-        for x in range(n + 1):
-            count = 0
+        for x in range(1, n + 1):
+            if offset * 2 == x:
+                offset = x
+            dp[x] = 1 + dp[x - offset]
 
-            while x:
-                x = x & (x - 1)
-                count += 1
-
-            result.append(count)
-
-        return result
+        return dp
