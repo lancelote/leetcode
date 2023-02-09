afee18a3f987c598da1316db3206910625ed5ccd
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-02-09 20:16:46 +0100

    Remove needless parens

--- src/cells_with_odd_values_in_a_matrix.py
+++ src/cells_with_odd_values_in_a_matrix.py
@@ -2,7 +2,7 @@ class Solution:
     def oddCells(self, m: int, n: int, indices: list[list[int]]) -> int:
         matrix = [[0 for _ in range(n)] for _ in range(m)]
 
-        for (row_i, col_j) in indices:
+        for row_i, col_j in indices:
             for j in range(n):
                 matrix[row_i][j] += 1
             for i in range(m):
