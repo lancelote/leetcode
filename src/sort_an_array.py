85ef51e7970719c7953a8216fadb3f4839321686
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-04-30 17:45:27 +0200

    "Sort an Array" merge sort approach

--- src/sort_an_array.py
+++ src/sort_an_array.py
@@ -1,12 +1,43 @@
 class Solution:
+    def merge_sort(self, array: list[int], start: int, end: int) -> None:
+        if end - start <= 1:
+            return
+
+        middle = (start + end) // 2
+
+        self.merge_sort(array, start, middle)
+        self.merge_sort(array, middle, end)
+
+        self.merge(array, start, end)
+
+    def merge(self, array: list[int], start: int, end: int) -> None:
+        length = end - start
+        middle = (start + end) // 2
+
+        tmp = [0] * length
+
+        left = 0
+        right = 0
+
+        while start + left < middle and middle + right < end:
+            if array[start + left] < array[middle + right]:
+                tmp[left + right] = array[start + left]
+                left += 1
+            else:
+                tmp[left + right] = array[middle + right]
+                right += 1
+
+        while start + left < middle:
+            tmp[left + right] = array[start + left]
+            left += 1
+
+        while middle + right < end:
+            tmp[left + right] = array[middle + right]
+            right += 1
+
+        for i in range(start, end):
+            array[i] = tmp[i - start]
+
     def sortArray(self, nums: list[int]) -> list[int]:
-        for i in range(len(nums)):
-            j = i
-
-            while j > 0 and nums[j] < nums[j - 1]:
-                tmp = nums[j]
-                nums[j] = nums[j - 1]
-                nums[j - 1] = tmp
-                j -= 1
-
+        self.merge_sort(nums, 0, len(nums))
         return nums
