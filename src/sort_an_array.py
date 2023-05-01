ca9abb118cc6671066bc02718447fee77036da77
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-05-01 22:18:10 +0200

    "Sort an Array" quick-sort attempt

--- src/sort_an_array.py
+++ src/sort_an_array.py
@@ -1,43 +1,24 @@
 class Solution:
-    def merge_sort(self, array: list[int], start: int, end: int) -> None:
-        if end - start <= 1:
+    def quick_sort(self, array: list[int], start: int, end: int) -> None:
+        if end - start < 1:
             return
 
-        middle = (start + end) // 2
+        pivot = array[end]
 
-        self.merge_sort(array, start, middle)
-        self.merge_sort(array, middle, end)
-
-        self.merge(array, start, end)
-
-    def merge(self, array: list[int], start: int, end: int) -> None:
-        length = end - start
-        middle = (start + end) // 2
-
-        tmp = [0] * length
-
-        left = 0
-        right = 0
-
-        while start + left < middle and middle + right < end:
-            if array[start + left] < array[middle + right]:
-                tmp[left + right] = array[start + left]
-                left += 1
-            else:
-                tmp[left + right] = array[middle + right]
-                right += 1
-
-        while start + left < middle:
-            tmp[left + right] = array[start + left]
-            left += 1
-
-        while middle + right < end:
-            tmp[left + right] = array[middle + right]
-            right += 1
-
+        slow = start
         for i in range(start, end):
-            array[i] = tmp[i - start]
+            if array[i] < pivot:
+                tmp = array[i]
+                array[i] = array[slow]
+                array[slow] = tmp
+                slow += 1
+
+        array[end] = array[slow]
+        array[slow] = pivot
+
+        self.quick_sort(array, start, slow - 1)
+        self.quick_sort(array, slow + 1, end)
 
     def sortArray(self, nums: list[int]) -> list[int]:
-        self.merge_sort(nums, 0, len(nums))
+        self.quick_sort(nums, start=0, end=len(nums) - 1)
         return nums
