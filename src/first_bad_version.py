33e297e0c997f7f4ca1bf73affe74b884fba3413
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-05-09 21:57:28 +0200

    Solve "First Bad Version" again

--- src/first_bad_version.py
+++ src/first_bad_version.py
@@ -4,12 +4,14 @@ def isBadVersion(version: int) -> int:
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        left, right = 1, n
+        left = 0
+        right = n
 
         while left < right:
             middle = left + (right - left) // 2
+            is_bad = isBadVersion(middle)
 
-            if isBadVersion(middle):
+            if is_bad:
                 right = middle
             else:
                 left = middle + 1
