5b498af811e36b873b9743e89afcfe9706cbf7a7
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-05-14 15:28:52 +0200

    Re-solve "Kids With the Greatest Number of Candies"

--- src/kids_with_the_greatest_number_of_candies.py
+++ src/kids_with_the_greatest_number_of_candies.py
@@ -1,4 +1,4 @@
 class Solution:
     def kidsWithCandies(self, kids: list[int], extra: int) -> list[bool]:
-        smallest_to_pass = max(kids) - extra
-        return [kid >= smallest_to_pass for kid in kids]
+        diff_to_pass = max(kids) - extra
+        return [kid >= diff_to_pass for kid in kids]
