10959296208df94f428eca3c351b0600def38e37
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-05-28 18:49:05 +0200

    Re-solve "Move Zeroes"

--- src/move_zeroes.py
+++ src/move_zeroes.py
@@ -1,8 +1,8 @@
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
-        # mutate
-        left = 0
-        for right, x in enumerate(nums):
-            if x:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
+        slow = 0
+
+        for fast, x in enumerate(nums):
+            if x != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
