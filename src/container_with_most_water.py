ad54adccfb57e6a5ca9da377dcfcdb25a475123c
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-05-30 21:31:09 +0200

    Re-solve "Container With Most Water"

--- src/container_with_most_water.py
+++ src/container_with_most_water.py
@@ -2,18 +2,17 @@ class Solution:
     def maxArea(self, height: list[int]) -> int:
         left = 0
         right = len(height) - 1
-        max_amount = 0
+        biggest_area = 0
 
         while left < right:
-            left_value = height[left]
-            right_value = height[right]
+            left_bar = height[left]
+            right_bar = height[right]
+            current_area = min(left_bar, right_bar) * (right - left)
+            biggest_area = max(biggest_area, current_area)
 
-            amount = min(left_value, right_value) * (right - left)
-            max_amount = max(max_amount, amount)
-
-            if left_value > right_value:
-                right -= 1
-            else:
+            if left_bar < right_bar:
                 left += 1
+            else:
+                right -= 1
 
-        return max_amount
+        return biggest_area
--- tests/test_container_with_most_water.py
+++ tests/test_container_with_most_water.py
@@ -8,6 +8,7 @@ from src.container_with_most_water import Solution
     [
         ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
         ([1, 1], 1),
+        ([1, 3, 2, 5, 25, 24, 5], 24),
     ],
 )
 def test_solution(height, expected):
