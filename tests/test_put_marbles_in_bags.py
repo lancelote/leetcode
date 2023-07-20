66aebab23b50e144ea28c828e1aa90a499ef25e3
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-07-20 22:48:21 +0200

    Fix tests for "Put Marbles in Bags"

--- tests/test_put_marbles_in_bags.py
+++ tests/test_put_marbles_in_bags.py
@@ -6,8 +6,8 @@ from src.put_marbles_in_bags import Solution
 @pytest.mark.parametrize(
     "weights,k,expected",
     (
-        ([1, 3, 5, 1], 2, 2),
-        ([1, 3], 2, 2),
+        ([1, 3, 5, 1], 2, 4),
+        ([1, 3], 2, 0),
     ),
 )
 def test_solution(weights, k, expected):
