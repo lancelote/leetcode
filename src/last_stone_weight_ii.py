42a0517a0bc3c6aaa057282575b606d1a137b4fd
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-08-02 23:34:38 +0200

    "Last Stone Weight II" better solution

--- src/last_stone_weight_ii.py
+++ src/last_stone_weight_ii.py
@@ -1,16 +1,19 @@
 class Solution:
     def lastStoneWeightII(self, stones: list[int]) -> int:
-        cache: dict[tuple[int, int, int], int] = {}
+        total = sum(stones)
+        half = total // 2
+        cache: dict[tuple[int, int], int] = {}
 
-        def dfs(i: int, left: int, right: int) -> int:
-            if i == len(stones):
-                return abs(left - right)
-            if (i, left, right) in cache:
-                return cache[(i, left, right)]
-            cache[(i, left, right)] = min(
-                dfs(i + 1, left + stones[i], right),
-                dfs(i + 1, left, right + stones[i]),
+        def dfs(i: int, pile: int) -> int:
+            if i == len(stones) or pile >= half:
+                return abs(pile - (total - pile))
+            if (i, pile) in cache:
+                return cache[(i, pile)]
+
+            cache[(i, pile)] = min(
+                dfs(i + 1, pile), dfs(i + 1, pile + stones[i])
             )
-            return cache[(i, left, right)]
 
-        return dfs(0, 0, 0)
+            return cache[(i, pile)]
+
+        return dfs(0, 0)
--- tests/test_last_stone_weight_ii.py
+++ tests/test_last_stone_weight_ii.py
@@ -8,6 +8,7 @@ from src.last_stone_weight_ii import Solution
     (
         ([2, 7, 4, 1, 8, 1], 1),
         ([31, 26, 33, 21, 40], 5),
+        ([1, 1], 0),
     ),
 )
 def test_solution(stones, expected):
