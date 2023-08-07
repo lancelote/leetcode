6f1b54df2e889c9b1f5b365215275d072af69f6d
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-08-07 23:38:09 +0200

    Re-solve "Coin Change"

--- src/coin_change.py
+++ src/coin_change.py
@@ -1,15 +1,12 @@
-from collections import defaultdict
-
-
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
-        dp: dict[int, int] = defaultdict(lambda: amount + 1)
+        coins = sorted(coins)
+        dp = [amount + 1] * (amount + 1)
         dp[0] = 0
 
-        for sub_amount in range(1, amount + 1):
+        for current in range(1, amount + 1):
             for coin in coins:
-                diff = sub_amount - coin
-                if diff >= 0:
-                    dp[sub_amount] = min(dp[sub_amount], 1 + dp[diff])
+                if current - coin >= 0:
+                    dp[current] = min(dp[current], 1 + dp[current - coin])
 
         return dp[amount] if dp[amount] != amount + 1 else -1
