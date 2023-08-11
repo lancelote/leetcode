953c9788fbb821f162b26629e9d378c06f0f5299
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-08-11 22:43:51 +0200

    Re-solve "Coin Change II"

--- src/coin_change_ii.py
+++ src/coin_change_ii.py
@@ -8,7 +8,10 @@ class Solution:
             new_dp[0] = 1
 
             for i in range(1, amount + 1):
-                new_dp[i] = dp[i] + (new_dp[i - coin] if i - coin >= 0 else 0)
+                new_dp[i] = dp[i]
+
+                if i - coin >= 0:
+                    new_dp[i] += new_dp[i - coin]
 
             dp = new_dp
 
