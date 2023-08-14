40a86613f623a49960bd4d671309ec62ec0e2a88
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-08-14 23:29:08 +0200

    Re-solve "Kth Largest Element in an Array"

--- src/kth_largest_element_in_an_array.py
+++ src/kth_largest_element_in_an_array.py
@@ -1,31 +1,14 @@
+import heapq
+
+
 class Solution:
-    def quick_select(
-        self, array: list[int], start: int, end: int, k: int
-    ) -> int:
-        n = len(array)
-
-        if end - start < 1:
-            return array[start]
-
-        pivot = array[end]
-        slow = start
-
-        for i in range(start, end):
-            if array[i] < pivot:
-                tmp = array[slow]
-                array[slow] = array[i]
-                array[i] = tmp
-                slow += 1
-
-        array[end] = array[slow]
-        array[slow] = pivot
-
-        if n - slow == k:
-            return pivot
-        elif n - slow > k:
-            return self.quick_select(array, slow + 1, end, k)
-        else:
-            return self.quick_select(array, start, slow - 1, k)
-
     def findKthLargest(self, nums: list[int], k: int) -> int:
-        return self.quick_select(nums, 0, len(nums) - 1, k)
+        heap: list[int] = nums[:k]
+        heapq.heapify(heap)
+
+        for i in range(k, len(nums)):
+            if nums[i] > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, nums[i])
+
+        return heap[0]
--- tests/test_kth_largest_element_in_an_array.py
+++ tests/test_kth_largest_element_in_an_array.py
@@ -10,6 +10,7 @@ from src.kth_largest_element_in_an_array import Solution
         ([3, 2, 3, 1, 2, 4, 5, 5, 6], 4, 4),
         ([1], 1, 1),
         ([2, 1], 1, 2),
+        ([-1, -1], 2, -1),
     ],
 )
 def test_solution(nums, k, expected):
