1a3707f5a0b3a346d2fecbb90dc5eb396590a5bb
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-08-24 10:02:51 +0200

    Re-solve "Unique Paths"

--- src/unique_paths.py
+++ src/unique_paths.py
@@ -1,15 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        cache = [[0] * n for _ in range(m)]
+        dp = [0] * (n + 1)
+        dp[-2] = 1
 
-        def dfs(r: int, c: int) -> int:
-            if r == m or c == n:
-                return 0
-            if r == m - 1 and c == n - 1:
-                return 1
-            if cache[r][c]:
-                return cache[r][c]
-            cache[r][c] = dfs(r + 1, c) + dfs(r, c + 1)
-            return cache[r][c]
+        for i in range(m - 1, -1, -1):
+            new_dp = [0] * (n + 1)
 
-        return dfs(0, 0)
+            for j in range(n - 1, -1, -1):
+                new_dp[j] = new_dp[j + 1] + dp[j]
+
+            dp = new_dp
+
+        return dp[0]
