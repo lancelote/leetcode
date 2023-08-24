b340e543acf78a73d06724c4c4fd1903c4532d71
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-08-24 09:57:12 +0200

    Re-solve "Longest Common Subsequence"

--- src/longest_common_subsequence.py
+++ src/longest_common_subsequence.py
@@ -1,19 +1,19 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n = len(text1)
-        m = len(text2)
+        n1 = len(text1)
+        n2 = len(text2)
 
-        dp = [0] * (m + 1)
+        dp = [0] * (n2 + 1)
 
-        for i in range(n):
-            new_dp = [0] * (m + 1)
+        for i1 in range(n1 - 1, -1, -1):
+            new_dp = [0] * (n2 + 1)
 
-            for j in range(m):
-                if text1[i] == text2[j]:
-                    new_dp[j + 1] = 1 + dp[j]
+            for i2 in range(n2 - 1, -1, -1):
+                if text1[i1] == text2[i2]:
+                    new_dp[i2] = 1 + dp[i2 + 1]
                 else:
-                    new_dp[j + 1] = max(dp[j + 1], new_dp[j])
+                    new_dp[i2] = max(dp[i2], new_dp[i2 + 1])
 
             dp = new_dp
 
-        return dp[-1]
+        return dp[0]
