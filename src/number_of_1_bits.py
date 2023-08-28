af61169635c28826957c7d0eb9e305917c78ab84
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-08-28 23:14:35 +0200

    Re-solve "Number of 1 Bits"

--- src/number_of_1_bits.py
+++ src/number_of_1_bits.py
@@ -1,10 +1,10 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        ones_count = 0
+        count = 0
 
-        while n:
+        while n != 0:
             if n & 1:
-                ones_count += 1
+                count += 1
             n >>= 1
 
-        return ones_count
+        return count
