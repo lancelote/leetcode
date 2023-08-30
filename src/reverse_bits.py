c8c43c3eb524597ef622bedc58b6b4a1554a959b
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-08-30 17:30:17 +0200

    Re-solve "Reverse Bits"

--- src/reverse_bits.py
+++ src/reverse_bits.py
@@ -1,28 +1,8 @@
-def clear_bit(i: int, n: int) -> int:
-    return n & ~(1 << i)
-
-
-def set_bit(i: int, n: int) -> int:
-    return n | 1 << i
-
-
 class Solution:
     def reverseBits(self, n: int) -> int:
-        for i in range(16):
-            lsbi = i
-            msbi = 31 - i
+        result = 0
 
-            lsb = n & 1 << lsbi
-            msb = n & 1 << msbi
+        for shift in range(32):
+            result |= (n & 1 << shift != 0) << (31 - shift)
 
-            if lsb:
-                n = set_bit(msbi, n)
-            else:
-                n = clear_bit(msbi, n)
-
-            if msb:
-                n = set_bit(lsbi, n)
-            else:
-                n = clear_bit(lsbi, n)
-
-        return n
+        return result
