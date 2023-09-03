ba600589305903f6f8b1f86b77e808a4f0546fcc
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-09-03 14:08:53 +0200

    Re-solve "Unique Paths"

--- src/unique_paths.py
+++ src/unique_paths.py
@@ -1,12 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [0] * (n + 1)
-        dp[-2] = 1
+        dp = [1] * n
 
-        for i in range(m - 1, -1, -1):
-            new_dp = [0] * (n + 1)
+        for i in range(m - 2, -1, -1):
+            new_dp = [0] * n
+            new_dp[-1] = 1
 
-            for j in range(n - 1, -1, -1):
+            for j in range(n - 2, -1, -1):
                 new_dp[j] = new_dp[j + 1] + dp[j]
 
             dp = new_dp
