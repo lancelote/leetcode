2657a19b045d386c5be6215dc290714916fb5041
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-09-22 09:39:19 +0200

    Re-solve "Is Subsequence"

--- src/is_subsequence.py
+++ src/is_subsequence.py
@@ -1,10 +1,15 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i = j = 0
+        n = len(s)
+        m = len(t)
 
-        while i < len(s) and j < len(t):
-            if s[i] == t[j]:
-                i += 1
-            j += 1
+        i1 = i2 = 0
 
-        return i == len(s)
+        while i1 < n and i2 < m:
+            if s[i1] == t[i2]:
+                i1 += 1
+                i2 += 1
+            else:
+                i2 += 1
+
+        return i1 == n
