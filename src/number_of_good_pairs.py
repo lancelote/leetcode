55fa025ed74f5f34b2fd44f414fd5d3d4b39c55e
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-10-03 20:34:39 +0200

    Re-solve "Number of Good Pairs"

--- src/number_of_good_pairs.py
+++ src/number_of_good_pairs.py
@@ -1,15 +1,7 @@
-from collections import defaultdict
-from typing import DefaultDict
+from collections import Counter
 
 
 class Solution:
     def numIdenticalPairs(self, nums: list[int]) -> int:
-        """A pair (i,j) is called good if nums[i] == nums[j] and i < j."""
-        good_pairs = 0
-        duplicates: DefaultDict[int, int] = defaultdict(int)
-
-        for num in nums:
-            good_pairs += duplicates[num]
-            duplicates[num] += 1
-
-        return good_pairs
+        count = Counter(nums)
+        return sum(x * (x - 1) // 2 for x in count.values())
