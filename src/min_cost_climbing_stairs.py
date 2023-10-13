e75daf1149d21ded2d4bf735ebb22bb402cd5465
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-10-13 23:36:21 +0200

    Re-solve "Min Cost Climbing Stairs"

--- src/min_cost_climbing_stairs.py
+++ src/min_cost_climbing_stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: list[int]) -> int:
-        a = b = 0
+        a, b = cost[0], cost[1]
 
-        for i in range(len(cost) - 1, -1, -1):
-            a, b = cost[i] + min(a, b), a
+        for i in range(2, len(cost)):
+            a, b = b, cost[i] + min(a, b)
 
         return min(a, b)
