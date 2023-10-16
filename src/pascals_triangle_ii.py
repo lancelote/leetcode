903b517e9bb757bd0deff2c63a184b84249b90d4
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-10-16 22:54:58 +0200

    Re-solve "Pascal's Triangle II"

--- src/pascals_triangle_ii.py
+++ src/pascals_triangle_ii.py
@@ -1,16 +1,14 @@
 class Solution:
     def getRow(self, row_index: int) -> list[int]:
-        last_row = [1]
+        row = [1]
 
         for _ in range(row_index):
-            new_row = []
+            new_row = [1]
 
-            for i in range(len(last_row) + 1):
-                item = 0
-                item += last_row[i - 1] if 0 <= i - 1 else 0
-                item += last_row[i] if i < len(last_row) else 0
-                new_row.append(item)
+            for i in range(len(row)):
+                new_item = row[i] + (row[i + 1] if i + 1 < len(row) else 0)
+                new_row.append(new_item)
 
-            last_row = new_row
+            row = new_row
 
-        return last_row
+        return row
