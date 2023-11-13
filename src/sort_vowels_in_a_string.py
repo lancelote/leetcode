dcef2f7176c82c7ed22329fb6daf75d9295b86ae
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-11-13 22:22:53 +0100

    Re-solve "Sort Vowels in a String"

--- src/sort_vowels_in_a_string.py
+++ src/sort_vowels_in_a_string.py
@@ -3,19 +3,15 @@ VOWELS = {"a", "e", "i", "o", "u"}
 
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vowels: list[str] = []
+        result = list(s)
 
-        for x in s:
-            if x.lower() in VOWELS:
-                vowels.append(x)
+        vowels = sorted(x for x in s if x.lower() in VOWELS)
 
-        vowels.sort(reverse=True)
-        result: list[str] = []
-
-        for x in s:
-            if x.lower() in VOWELS:
-                result.append(vowels.pop())
-            else:
-                result.append(x)
+        i = 0
+        for vowel in vowels:
+            while s[i].lower() not in VOWELS:
+                i += 1
+            result[i] = vowel
+            i += 1
 
         return "".join(result)
