1375d4f684e83fe0c7a8695dbfc8b60532aa6c00
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-11-26 19:19:06 +0100

    Re-solve "Two Sum"

--- src/two_sum.py
+++ src/two_sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        processed: dict[int, int] = {}
+        seen: dict[int, int] = {}
 
-        for i, first in enumerate(nums):
-            if (second := target - first) in processed:
-                return [processed[second], i]
-            processed[first] = i
+        for i, x in enumerate(nums):
+            compliment = target - x
+            if compliment in seen:
+                return [seen[compliment], i]
+            seen[x] = i
 
-        return []
+        raise ValueError
