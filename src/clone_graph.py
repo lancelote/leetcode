8daddf55e7b635c6304d31675ac960b035bb82ef
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-12-22 17:48:39 +0100

    Re-solve "Clone Graph"

--- src/clone_graph.py
+++ src/clone_graph.py
@@ -3,25 +3,25 @@ from src.utils.undirected_graph import Node
 
 class Solution:
     def cloneGraph(self, root: Node | None) -> Node | None:
-        node_map: dict[int, Node] = {}
+        index_to_node: dict[int, Node] = {}
 
-        def clone(node: Node | None) -> Node | None:
+        def dfs(node: Node | None) -> Node | None:
             if not node:
                 return None
 
-            if node.val in node_map:
-                return node_map[node.val]
+            if node.val in index_to_node:
+                return index_to_node[node.val]
 
-            new_node = Node(val=node.val)
-            node_map[node.val] = new_node
-
+            new_node = Node(node.val)
+            index_to_node[new_node.val] = new_node
             neighbors: list[Node] = []
-            for child in node.neighbors:
-                clone_child = clone(child)
-                if clone_child:
-                    neighbors.append(clone_child)
-            new_node.neighbors = neighbors
 
+            for neighbor in node.neighbors:
+                new_neighbor = dfs(neighbor)
+                if new_neighbor:
+                    neighbors.append(new_neighbor)
+
+            new_node.neighbors = neighbors
             return new_node
 
-        return clone(root)
+        return dfs(root)
