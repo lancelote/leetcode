08aa806b9d534f609c43efc55e4dd126b47669e5
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-01-11 21:17:33 +0100

    Re-solve "Counting Bits"

--- src/counting_bits.py
+++ src/counting_bits.py
@@ -1,12 +1,8 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
         result = [0] * (n + 1)
-        shift = 1
 
-        for i in range(1, n + 1):
-            if shift * 2 == i:
-                shift = i
-
-            result[i] = 1 + result[i - shift]
+        for x in range(1, n + 1):
+            result[x] = 1 + result[x & (x - 1)]
 
         return result
