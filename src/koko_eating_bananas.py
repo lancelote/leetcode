fb40278986cf6e0a8f03db69ad7bd919b5179874
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-01-14 13:10:59 +0100

    Re-solve "Koko Eating Bananas"

--- src/koko_eating_bananas.py
+++ src/koko_eating_bananas.py
@@ -2,22 +2,22 @@ import math
 
 
 class Solution:
-    def will_take(self, k: int, piles: list[int]) -> int:
-        return sum(int(math.ceil(pile / k)) for pile in piles)
+    def will_take(self, mid: int, piles: list[int]) -> int:
+        return sum(int(math.ceil(pile / mid)) for pile in piles)
 
     def minEatingSpeed(self, piles: list[int], h: int) -> int:
         left = 1
         right = max(piles)
-        min_k = right
+        min_mid = right
 
         while left <= right:
-            k = left + (right - left) // 2
-            eating_h = self.will_take(k, piles)
+            mid = (left + right) // 2
+            t = self.will_take(mid, piles)
 
-            if eating_h > h:
-                left = k + 1
+            if t > h:
+                left = mid + 1
             else:
-                right = k - 1
-                min_k = min(min_k, k)
+                right = mid - 1
+                min_mid = min(min_mid, mid)
 
-        return min_k
+        return min_mid
