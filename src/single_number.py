e6f87eb2a65f4744642e8802ab08290a3f76b86a
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-01-19 12:01:36 +0100

    Re-solve "Single Number"

--- src/single_number.py
+++ src/single_number.py
@@ -1,8 +1,6 @@
+from functools import reduce
+
+
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
-        result = 0
-
-        for x in nums:
-            result ^= x
-
-        return result
+        return reduce(lambda x, y: x ^ y, nums)
