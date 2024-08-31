c7793e5edb5f32ab8cfb9ffcdb13ab242995dee1
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-08-31 21:30:51 +0200

    Re-solve "Ransom Note"

--- src/ransom_note.py
+++ src/ransom_note.py
@@ -1,16 +1,13 @@
-import typing
-from collections import defaultdict
+from collections import Counter
 
 
 class Solution:
     def canConstruct(self, ransom_note: str, magazine: str) -> bool:
-        char_counter: typing.DefaultDict[str, int] = defaultdict(int)
+        available = Counter(magazine)
 
-        for char in magazine:
-            char_counter[char] += 1
-
-        for char in ransom_note:
-            if char_counter[char] == 0:
+        for letter in ransom_note:
+            if available[letter] == 0:
                 return False
-            char_counter[char] -= 1
+            available[letter] -= 1
+
         return True
