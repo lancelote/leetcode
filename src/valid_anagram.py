08a7ad4d4c074a7d85631a184e3c5510e2c863cf
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-09-03 15:19:47 +0200

    Re-solve "242. Valid Anagram"

--- src/valid_anagram.py
+++ src/valid_anagram.py
@@ -3,6 +3,4 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
         return Counter(s) == Counter(t)
