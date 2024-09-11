18688ead34891c6735f7ff073e14458684ea7fe8
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-09-11 23:11:50 +0200

    re-solve "58. Length of Last Word"

--- src/length_of_last_word.py
+++ src/length_of_last_word.py
@@ -3,9 +3,9 @@ class Solution:
         length = 0
 
         for i in range(len(s) - 1, -1, -1):
-            if s[i] != " ":
+            if s[i].isalpha():
                 length += 1
             elif length:
-                return length
+                break
 
         return length
