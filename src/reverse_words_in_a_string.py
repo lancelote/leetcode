09c6d4352b79e04ade2ab9da1d976c9c83f9f23f
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-09-12 23:42:20 +0200

    re-solve "151. Reverse Words in a String"

--- src/reverse_words_in_a_string.py
+++ src/reverse_words_in_a_string.py
@@ -1,25 +1,22 @@
+from collections.abc import Iterator
+
+
+def iter_words(s: str) -> Iterator[str]:
+    buffer: list[str] = []
+
+    for letter in s:
+        if letter.isspace():
+            if buffer:
+                yield "".join(buffer)
+                buffer.clear()
+        else:
+            buffer.append(letter)
+
+    if buffer:
+        yield "".join(buffer)
+
+
 class Solution:
     def reverseWords(self, s: str) -> str:
-        result: list[str] = []
-
-        word: list[str] = []
-
-        for char in s:
-            if char == " " and word:
-                result.append("".join(word))
-                word = []
-            elif char != " ":
-                word.append(char)
-
-        if word:
-            result.append("".join(word))
-
-        left = 0
-        right = len(result) - 1
-
-        while left < right:
-            result[left], result[right] = result[right], result[left]
-            left += 1
-            right -= 1
-
-        return " ".join(result)
+        words = list(iter_words(s))
+        return " ".join(reversed(words))
--- tests/test_reverse_words_in_a_string.py
+++ tests/test_reverse_words_in_a_string.py
@@ -9,6 +9,7 @@ from src.reverse_words_in_a_string import Solution
         ("the sky is blue", "blue is sky the"),
         ("  hello world  ", "world hello"),
         ("a good   example", "example good a"),
+        ("EPY2giL", "EPY2giL"),
     ],
 )
 def test_solution(s, expected):
