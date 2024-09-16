23f6d1677e6cbb73deb1241f5e7eafb0ee644506
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-09-16 22:42:16 +0200

    re-solve "28. Find the Index of the First Occurrence in a String" using Knuth–Morris–Pratt algorithm

--- src/find_the_index_of_the_first_occurrence_in_a_string.py
+++ src/find_the_index_of_the_first_occurrence_in_a_string.py
@@ -1,9 +1,34 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack) - len(needle) + 1):
-            for j in range(len(needle)):
-                if needle[j] != haystack[i + j]:
-                    break
+        lps = [0] * len(needle)
+
+        prev_lps = 0
+        i = 1
+
+        while i < len(needle):
+            if needle[i] == needle[prev_lps]:
+                lps[i] = prev_lps + 1
+                prev_lps += 1
+                i += 1
+            elif prev_lps == 0:
+                lps[i] = 0
+                i += 1
             else:
-                return i
+                prev_lps = lps[prev_lps - 1]
+
+        i = 0  # ptr for haystack
+        j = 0  # ptr for needle
+
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+            else:
+                if j == 0:
+                    i += 1
+                else:
+                    j = lps[j - 1]
+            if j == len(needle):
+                return i - len(needle)
+
         return -1
