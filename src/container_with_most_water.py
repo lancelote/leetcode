136a5168fcca5fabf133f6821ffdeddaf611015e
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-09-26 22:56:55 +0200

    re-solve "11. Container With Most Water"

--- src/container_with_most_water.py
+++ src/container_with_most_water.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        biggest_area = 0
+        left, right = 0, len(height) - 1
+
+        max_area = 0
 
         while left < right:
-            left_bar = height[left]
-            right_bar = height[right]
-            current_area = min(left_bar, right_bar) * (right - left)
-            biggest_area = max(biggest_area, current_area)
+            area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, area)
 
-            if left_bar < right_bar:
+            if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
 
-        return biggest_area
+        return max_area
