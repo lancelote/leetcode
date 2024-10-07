fa7bedd6e07af61eabefde59652e91a5215caa02
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-10-07 22:16:53 +0200

    re-solve "36. Valid Sudoku"

--- src/valid_sudoku.py
+++ src/valid_sudoku.py
@@ -1,32 +1,45 @@
-from collections.abc import Iterator
-from itertools import chain
-
-
-def to_int_seq(seq: list[str]) -> list[int]:
-    return [int(x) for x in seq if x != "."]
-
-
-def rows(board: list[list[str]]) -> Iterator[list[int]]:
-    for row in board:
-        yield to_int_seq(row)
-
-
-def cols(board: list[list[str]]) -> Iterator[list[int]]:
-    for col_i in range(9):
-        col = [row[col_i] for row in board]
-        yield to_int_seq(col)
-
-
-def squares(board: list[list[str]]) -> Iterator[list[int]]:
-    for i in range(3):
-        for j in range(3):
-            square = [board[3 * i + k // 3][3 * j + k % 3] for k in range(9)]
-            yield to_int_seq(square)
-
-
 class Solution:
-    def isValidSudoku(self, board: list[list[str]]) -> bool:
-        for seq in chain(rows(board), cols(board), squares(board)):
-            if len(seq) != len(set(seq)):
-                return False
+    @staticmethod
+    def are_rows_valid(board: list[list[str]]) -> bool:
+        for row in board:
+            seen: set[str] = set()
+
+            for x in row:
+                if x != ".":
+                    if x in seen:
+                        return False
+                    seen.add(x)
         return True
+
+    @staticmethod
+    def are_cols_valid(board: list[list[str]]) -> bool:
+        for c in range(9):
+            seen: set[str] = set()
+
+            for r in range(9):
+                if (x := board[r][c]) != ".":
+                    if x in seen:
+                        return False
+                    seen.add(x)
+        return True
+
+    @staticmethod
+    def are_squares_valid(board: list[list[str]]) -> bool:
+        for sr in range(3):
+            for sc in range(3):
+                seen: set[str] = set()
+
+                for r in range(3):
+                    for c in range(3):
+                        if (x := board[r + 3 * sr][c + 3 * sc]) != ".":
+                            if x in seen:
+                                return False
+                            seen.add(x)
+        return True
+
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        return (
+            self.are_rows_valid(board)
+            and self.are_cols_valid(board)
+            and self.are_squares_valid(board)
+        )
