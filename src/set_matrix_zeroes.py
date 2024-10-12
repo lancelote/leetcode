9a60b25ba05aa2aa17efead698eb63c1564d2b70
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-10-12 18:31:20 +0200

    re-solve "73. Set Matrix Zeroes"

--- src/set_matrix_zeroes.py
+++ src/set_matrix_zeroes.py
@@ -1,39 +1,24 @@
+def set_row_none(matrix: list[list[int | None]], r: int) -> None:
+    for c in range(len(matrix[0])):
+        if matrix[r][c] != 0:
+            matrix[r][c] = None
+
+
+def set_col_none(matrix: list[list[int | None]], c: int) -> None:
+    for row in matrix:
+        if row[c] != 0:
+            row[c] = None
+
+
 class Solution:
-    def setZeroes(self, matrix: list[list[int]]) -> None:
-        assert matrix
-        assert matrix[0]
+    def setZeroes(self, matrix: list[list[int | None]]) -> None:
+        for r, row in enumerate(matrix):
+            for c, item in enumerate(row):
+                if item == 0:
+                    set_row_none(matrix, r)
+                    set_col_none(matrix, c)
 
-        rows = len(matrix)
-        cols = len(matrix[0])
-        first_row = matrix[0][0]
-
-        for c in range(cols):
-            if matrix[0][c] == 0:
-                first_row = 0
-                break
-
-        for c in range(cols):
-            for r in range(rows):
-                if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                    break
-
-        for r in range(1, rows):
-            for c in range(cols):
-                if matrix[r][c] == 0:
-                    matrix[r][0] = 0
-                    break
-
-        for r in range(1, rows):
-            if matrix[r][0] == 0:
-                for c in range(1, cols):
+        for r, row in enumerate(matrix):
+            for c, item in enumerate(row):
+                if item is None:
                     matrix[r][c] = 0
-
-        for c in range(cols):
-            if matrix[0][c] == 0:
-                for r in range(1, rows):
-                    matrix[r][c] = 0
-
-        if first_row == 0:
-            for c in range(cols):
-                matrix[0][c] = 0
