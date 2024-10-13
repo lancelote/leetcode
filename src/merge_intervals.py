bdb9d9d419cad0d63da7b1d6d422bbf83c53d51a
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-10-13 13:31:02 +0200

    re-solve "56. Merge Intervals"

--- src/merge_intervals.py
+++ src/merge_intervals.py
@@ -1,22 +1,15 @@
 class Solution:
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
-        assert intervals
-
         n = len(intervals)
-        result: list[list[int]] = []
-        intervals = sorted(intervals, key=lambda x: x[0])
-
-        tmp_start, tmp_end = intervals[0]
+        sorted_intervals = sorted(intervals)
+        result: list[list[int]] = [sorted_intervals[0]]
 
         for i in range(1, n):
-            start, end = intervals[i]
+            left, right = sorted_intervals[i]
 
-            if start <= tmp_end:
-                tmp_end = max(end, tmp_end)
+            if result[-1][1] >= left:
+                result[-1][1] = max(result[-1][1], right)
             else:
-                result.append([tmp_start, tmp_end])
-                tmp_start = start
-                tmp_end = end
+                result.append([left, right])
 
-        result.append([tmp_start, tmp_end])
         return result
