8302201530fd643fb991759f135cdce4cee41806
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-10-16 22:01:22 +0200

    re-solve "20. Valid Parentheses"

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,12 +1,19 @@
-TO_CLOSING = {"(": ")", "{": "}", "[": "]"}
+PARENS = {
+    "{": "}",
+    "(": ")",
+    "[": "]",
+}
 
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        expected_stack: list[str] = []
-        for paren in s:
-            if paren in TO_CLOSING:
-                expected_stack.append(TO_CLOSING[paren])
-            elif not expected_stack or expected_stack.pop() != paren:
-                return False
-        return not expected_stack
+        stack: list[str] = []
+
+        for x in s:
+            if x in PARENS:
+                stack.append(PARENS[x])
+            else:
+                if not stack or stack.pop() != x:
+                    return False
+
+        return not stack
