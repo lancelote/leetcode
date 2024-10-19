d9faf2cd47794667a134a5f1cfed286ef9958590
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-10-19 20:52:51 +0200

    re-solve "155. Min Stack"

--- src/min_stack.py
+++ src/min_stack.py
@@ -1,22 +1,23 @@
 class MinStack:
     def __init__(self) -> None:
-        self.val_stack: list[int] = []
-        self.min_stack: list[int] = []
+        self._val_stack: list[int] = []
+        self._min_stack: list[int] = []
 
     def push(self, val: int) -> None:
-        self.val_stack.append(val)
-        if not self.min_stack:
-            self.min_stack.append(val)
+        self._val_stack.append(val)
+
+        if not self._min_stack:
+            self._min_stack.append(val)
         else:
-            smallest = min(self.min_stack[-1], val)
-            self.min_stack.append(smallest)
+            new_min = min(self._min_stack[-1], val)
+            self._min_stack.append(new_min)
 
     def pop(self) -> None:
-        self.val_stack.pop()
-        self.min_stack.pop()
+        self._val_stack.pop()
+        self._min_stack.pop()
 
     def top(self) -> int:
-        return self.val_stack[-1]
+        return self._val_stack[-1]
 
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        return self._min_stack[-1]
