d0c530738e46213dc0316c40f15ba48ff57c2327
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-11-04 13:50:37 +0100

    re-solve "101. Symmetric Tree"

--- src/symmetric_tree.py
+++ src/symmetric_tree.py
@@ -1,31 +1,17 @@
-from collections.abc import Iterator
-
 from src.utils.binary_tree import TreeNode
 
 
-def items_left(root: TreeNode | None) -> Iterator[int | None]:
-    yield root.val if root else None
-
-    if root:
-        yield from items_left(root.left)
-        yield from items_left(root.right)
-
-
-def items_right(root: TreeNode | None) -> Iterator[int | None]:
-    yield root.val if root else None
-
-    if root:
-        yield from items_right(root.right)
-        yield from items_right(root.left)
+def dfs(left: TreeNode | None, right: TreeNode | None) -> bool:
+    if not left and not right:
+        return True
+    elif not left or not right or left.val != right.val:
+        return False
+    else:
+        return dfs(left.left, right.right) and dfs(left.right, right.left)
 
 
 class Solution:
     def isSymmetric(self, root: TreeNode | None) -> bool:
-        if not root:
+        if root is None:
             return True
-
-        for x, y in zip(items_left(root.left), items_right(root.right)):
-            if x != y:
-                return False
-
-        return True
+        return dfs(root.left, root.right)
