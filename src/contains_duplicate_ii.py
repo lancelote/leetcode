89b959afc9859ac11f6392729980f3b4c2d7a0ac
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-01-16 17:12:48 +0100

    re-solve "219. Contains Duplicate II"

--- src/contains_duplicate_ii.py
+++ src/contains_duplicate_ii.py
@@ -1,10 +1,23 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        cache: dict[int, int] = {}
+        if k == 0:
+            return False
 
-        for i, num in enumerate(nums):
-            if num in cache and i - cache[num] <= k:
+        n = len(nums)
+
+        seen: set[int] = set()
+
+        for i in range(min(k, n)):
+            if nums[i] in seen:
                 return True
-            cache[num] = i
+
+            seen.add(nums[i])
+
+        for i in range(k, len(nums)):
+            if nums[i] in seen:
+                return True
+
+            seen.discard(nums[i - k])
+            seen.add(nums[i])
 
         return False
--- tests/test_contains_duplicate_ii.py
+++ tests/test_contains_duplicate_ii.py
@@ -9,6 +9,8 @@ from src.contains_duplicate_ii import Solution
         ([1, 2, 3, 1], 3, True),
         ([1, 0, 1, 1], 1, True),
         ([1, 2, 3, 1, 2, 3], 2, False),
+        ([1, 2, 1], 0, False),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15, False),
     ],
 )
 def test_solution(nums, k, expected):
