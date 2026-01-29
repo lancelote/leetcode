06117f371782cd40aa689abeb04fbdb379c09f5e
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-01-29 19:53:24 +0100

    re-solve "151. Reverse Words in a String"

--- src/reverse_words_in_a_string.py
+++ src/reverse_words_in_a_string.py
@@ -1,22 +1,3 @@
-from collections.abc import Iterator
-
-
-def iter_words(s: str) -> Iterator[str]:
-    buffer: list[str] = []
-
-    for letter in s:
-        if letter.isspace():
-            if buffer:
-                yield "".join(buffer)
-                buffer.clear()
-        else:
-            buffer.append(letter)
-
-    if buffer:
-        yield "".join(buffer)
-
-
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = list(iter_words(s))
-        return " ".join(reversed(words))
+        return " ".join(reversed(s.split()))
