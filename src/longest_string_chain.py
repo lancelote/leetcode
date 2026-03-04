a70febdd1c105d33b3f2013ed9af9e63ef88f196
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-03-04 17:14:19 +0100

    return back the cast - latest `ty` cannot handle that

--- src/longest_string_chain.py
+++ src/longest_string_chain.py
@@ -1,8 +1,11 @@
+from typing import cast
+
+
 class Solution:
     def longestStrChain(self, words: list[str]) -> int:
         dp: dict[str, int] = {}
 
-        for word in sorted(words, key=len):
+        for word in cast(list[str], sorted(words, key=len)):
             dp[word] = max(
                 dp.get(word[:i] + word[i + 1 :], 0) + 1
                 for i in range(len(word))
