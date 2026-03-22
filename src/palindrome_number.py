088279a358ffe11b1d557ec5a8f81e9139b5e1f1
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-03-22 20:45:14 +0100

    re-solve "Palindrome Number"

--- src/palindrome_number.py
+++ src/palindrome_number.py
@@ -1,4 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        str_x = str(x)
-        return str_x == str_x[::-1]
+        if x < 0 or x % 10 == 0 and x != 0:
+            return False
+
+        reverted_x = 0
+
+        while x > reverted_x:
+            reverted_x = reverted_x * 10 + x % 10
+            x //= 10
+
+        return x == reverted_x or x == reverted_x // 10
