e250b36d784881e8f80ec7ec7d1f64f317539f82
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-09 23:37:05 +0200

    re-solve "Symmetric Tree"

--- src/symmetric_tree.py
+++ src/symmetric_tree.py
@@ -1,17 +1,22 @@
 from src.utils.binary_tree import TreeNode
 
 
-def dfs(left: TreeNode | None, right: TreeNode | None) -> bool:
-    if not left and not right:
+def dfs(node1: TreeNode | None, node2: TreeNode | None) -> bool:
+    if node1 is None and node2 is None:
         return True
-    elif not left or not right or left.val != right.val:
+
+    if node1 is None or node2 is None:
         return False
-    else:
-        return dfs(left.left, right.right) and dfs(left.right, right.left)
+
+    if node1.val != node2.val:
+        return False
+
+    return dfs(node1.left, node2.right) and dfs(node1.right, node2.left)
 
 
 class Solution:
     def isSymmetric(self, root: TreeNode | None) -> bool:
         if root is None:
             return True
+
         return dfs(root.left, root.right)
