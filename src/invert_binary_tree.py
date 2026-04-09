186045e7566249b1e4da6f047c551dee7f8b2cd4
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-09 11:27:04 +0200

    re-solve "Invert Binary Tree"

--- src/invert_binary_tree.py
+++ src/invert_binary_tree.py
@@ -4,7 +4,7 @@ from src.utils.binary_tree import TreeNode
 class Solution:
     def invertTree(self, root: TreeNode | None) -> TreeNode | None:
         if root is None:
-            return None
+            return root
 
         root.left, root.right = root.right, root.left
 
