5bf8b0c82b5d0511d476ec5ada0cf38e8f93860c
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-11 21:54:02 +0200

    simplify "Construct Binary Tree from Inorder and Postorder Traversal"

--- src/construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ src/construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -5,19 +5,14 @@ class Solution:
     def buildTree(
         self, inorder: list[int], postorder: list[int]
     ) -> TreeNode | None:
-        post_idx = len(postorder) - 1
         val_to_in_idx = {x: i for i, x in enumerate(inorder)}
 
         def dfs(left: int, right: int) -> TreeNode | None:
-            nonlocal post_idx
-
             if left > right:
                 return None
 
-            val = postorder[post_idx]
+            val = postorder.pop()
             in_idx = val_to_in_idx[val]
-
-            post_idx -= 1
 
             node = TreeNode(val)
             node.right = dfs(in_idx + 1, right)
