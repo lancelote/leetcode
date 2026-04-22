fdea79b3177099901a0533aa1021f2c0480651df
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-22 19:55:00 +0200

    re-solve "14. Longest Common Prefix"

--- src/longest_common_prefix.py
+++ src/longest_common_prefix.py
@@ -1,16 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        assert len(strs) >= 1
-
-        prefix: list[str] = []
-
-        for i in range(len(strs[0])):
-            ch = strs[0][i]
-
+        for i, x in enumerate(strs[0]):
             for j in range(1, len(strs)):
-                if i >= len(strs[j]) or strs[j][i] != ch:
-                    return "".join(prefix)
-
-            prefix.append(ch)
-
-        return "".join(prefix)
+                if i >= len(strs[j]) or strs[j][i] != x:
+                    return strs[0][:i]
+        return strs[0]
