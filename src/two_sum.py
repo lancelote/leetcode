869adb1a5fc6e56b4e13d0fb6708e8c3d7f0e813
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-22 19:12:27 +0200

    re-solve "Two Sum"

--- src/two_sum.py
+++ src/two_sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        compliments: dict[int, int] = {}
+        compliment: dict[int, int] = {}
 
         for i, x in enumerate(nums):
-            compliment = target - x
-            if compliment in compliments:
-                return [compliments[compliment], i]
-            compliments[x] = i
+            if x in compliment:
+                return [compliment[x], i]
+            compliment[target - x] = i
 
-        raise ValueError("solution not found")
+        raise ValueError("no answer found")
