583c348ddba66cd3a39b0836fcb215400da1a90f
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-24 13:02:48 +0200

    re-solve "121. Best Time to Buy and Sell Stock"

--- src/best_time_to_buy_and_sell_stock.py
+++ src/best_time_to_buy_and_sell_stock.py
@@ -1,9 +1,7 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        assert prices
-
-        min_price = prices[0]
         max_profit = 0
+        min_price = prices[0]
 
         for price in prices:
             max_profit = max(max_profit, price - min_price)
