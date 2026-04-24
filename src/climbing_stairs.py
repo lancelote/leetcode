3f4b863e5b91b5479516c712a34afbdc5e2f3596
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-24 12:45:57 +0200

    re-solve "70. Climbing Stairs"

--- src/climbing_stairs.py
+++ src/climbing_stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        a = b = 1
+        a, b = 0, 1
 
-        for _ in range(n - 1):
+        for _ in range(1, n + 1):
             a, b = b, a + b
 
         return b
