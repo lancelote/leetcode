af885f7a91a61e1c26624d341c5ea08daa77bed3
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-25 22:30:02 +0200

    re-solve "1603. Design Parking System"

--- src/design_parking_system.py
+++ src/design_parking_system.py
@@ -1,10 +1,9 @@
 class ParkingSystem:
-    def __init__(self, big: int, medium: int, small: int):
-        self.lots = {1: big, 2: medium, 3: small}
+    def __init__(self, big: int, medium: int, small: int) -> None:
+        self.parking = {1: big, 2: medium, 3: small}
 
     def addCar(self, carType: int) -> bool:
-        if self.lots[carType] > 0:
-            self.lots[carType] -= 1
-            return True
-        else:
+        if self.parking[carType] == 0:
             return False
+        self.parking[carType] -= 1
+        return True
