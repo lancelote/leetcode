cd37278c52eee353ecbb94228a4095625adca82a
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-25 18:59:23 +0200

    re-solve "977. Squares of a Sorted Array"

--- src/squares_of_a_sorted_array.py
+++ src/squares_of_a_sorted_array.py
@@ -1,14 +1,14 @@
 class Solution:
     def sortedSquares(self, nums: list[int]) -> list[int]:
-        left, right = 0, len(nums) - 1
-        result = []
+        result: list[int] = []
 
+        left, right = 0, len(nums) - 1
         while left <= right:
-            if abs(nums[right]) > abs(nums[left]):
-                result.append(nums[right] ** 2)
-                right -= 1
-            else:
-                result.append(nums[left] ** 2)
+            if abs(nums[left]) > abs(nums[right]):
+                result.append(nums[left] * nums[left])
                 left += 1
+            else:
+                result.append(nums[right] * nums[right])
+                right -= 1
 
         return result[::-1]
