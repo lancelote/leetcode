c566a17329c680418faa3f3493b69d4dada9be7e
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-27 18:18:02 +0200

    re-solve "5. Longest Palindromic Substring"

--- src/longest_palindromic_substring.py
+++ src/longest_palindromic_substring.py
@@ -1,30 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        longest_sub = ""
-        longest_len = 0
+        result = [0, 0]
 
-        def check_palindrome(left: int, right: int) -> None:
-            nonlocal longest_sub
-            nonlocal longest_len
-
-            current = 0
-
-            while left >= 0 and right < n:
-                if s[left] == s[right]:
-                    current += 1 + (left != right)
-                else:
-                    break
-
-                if current > longest_len:
-                    longest_len = current
-                    longest_sub = s[left : right + 1]
-
+        def expand_palindrome(left: int, right: int) -> None:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left > result[1] - result[0]:
+                    result[0] = left
+                    result[1] = right
                 left -= 1
                 right += 1
 
-        for i in range(n):
-            check_palindrome(i, i)
-            check_palindrome(i, i + 1)
+        for i in range(len(s)):
+            expand_palindrome(i, i)
+            expand_palindrome(i, i + 1)
 
-        return longest_sub
+        start, end = result
+        return s[start : end + 1]
