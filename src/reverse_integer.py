d3be42ec59dd58e1b734bdf6b60182e4debe0bae
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-04-27 12:11:09 +0200

    re-solve "7. Reverse Integer"

--- src/reverse_integer.py
+++ src/reverse_integer.py
@@ -1,8 +1,22 @@
+import math
+
+LIMIT = 2**31 // 10
+
+
 class Solution:
     def reverse(self, x: int) -> int:
-        result = int(str(abs(x))[::-1])
+        result = 0
 
-        if -(2**31) - 1 < result < 2**31:
-            return -result if x < 0 else result
+        while x != 0:
+            digit = int(math.fmod(x, 10))
 
-        return 0
+            if abs(result) > LIMIT:
+                return 0
+
+            if abs(result) == LIMIT and ((digit >= 8 and x > 0) or digit == -9):
+                return 0
+
+            result = result * 10 + digit
+            x = math.trunc(x / 10)
+
+        return result
--- tests/test_reverse_integer.py
+++ tests/test_reverse_integer.py
@@ -9,6 +9,10 @@ from src.reverse_integer import Solution
         (123, 321),
         (-123, -321),
         (0, 0),
+        (1534236469, 0),
+        (-2147483412, -2143847412),
+        (-2147483648, 0),
+        (1463847412, 2147483641),
     ],
 )
 def test_solution(x, expected):
