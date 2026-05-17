994dfe78a3b4590de57b216b86ae945e3b2f647a
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-05-17 21:38:07 +0200

    re-solve "33. Search in Rotated Sorted Array"

--- src/search_in_rotated_sorted_array.py
+++ src/search_in_rotated_sorted_array.py
@@ -1,27 +1,22 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        assert nums
-
-        left = 0
-        right = len(nums) - 1
+        n = len(nums)
+        left, right = 0, n - 1
 
         while left <= right:
-            middle = (left + right) // 2
-            guess = nums[middle]
-            in_left_portion = guess >= nums[left]
-            is_too_small = guess < target
-            is_too_big = guess > target
+            mid = left + (right - left) // 2
 
-            if guess == target:
-                return middle
-            elif in_left_portion:
-                if is_too_small or target < nums[left]:
-                    left = middle + 1
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[left]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
                 else:
-                    right = middle - 1
+                    left = mid + 1
             else:
-                if is_too_big or target > nums[right]:
-                    right = middle - 1
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
                 else:
-                    left = middle + 1
+                    right = mid - 1
+
         return -1
