908ed8f142c99473d4d4cebf624f830dcdcdfdb1
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-05-22 21:28:10 +0200

    re-solve "34. Find First and Last Position of Element in Sorted Array"

--- src/find_first_and_last_position_of_element_in_sorted_array.py
+++ src/find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,68 +1,41 @@
-import math
-
-
-def find_any_index(nums: list[int], target: int) -> int:
-    n = len(nums)
-    left, right = 0, n
-
-    while left < right:
-        middle = (right - left) // 2 + left
-
-        if nums[middle] == target:
-            return middle
-        elif nums[middle] > target:
-            right = middle
-        else:
-            left = middle + 1
-
-    return -1
-
-
-def find_left_index(nums: list[int], any_index: int, target: int) -> int:
-    assert any_index != -1
-
-    left, right = 0, any_index
-
-    while left < right:
-        middle = (right - left) // 2 + left
-
-        if nums[middle] == target:
-            right = middle
-        elif nums[middle] > target:
-            raise ValueError
-        else:
-            left = middle + 1
-
-    return right
-
-
-def find_right_index(nums: list[int], any_index: int, target: int) -> int:
-    assert any_index != -1
-
-    n = len(nums)
-    left, right = any_index, n - 1
-
-    while left < right:
-        middle = math.ceil((right - left) / 2) + left
-
-        if nums[middle] == target:
-            left = middle
-        elif nums[middle] < target:
-            raise ValueError
-        else:
-            right = middle - 1
-
-    return left
-
-
 class Solution:
     def searchRange(self, nums: list[int], target: int) -> list[int]:
-        any_index = find_any_index(nums, target)
+        def find_left() -> int:
+            result = -1
+            li, ri = 0, len(nums) - 1
 
-        if any_index == -1:
+            while li <= ri:
+                m = li + (ri - li) // 2
+
+                if nums[m] == target:
+                    result = m
+                    ri = m - 1
+                elif nums[m] < target:
+                    li = m + 1
+                else:
+                    ri = m - 1
+
+            return result
+
+        def find_right() -> int:
+            result = -1
+            li, ri = 0, len(nums) - 1
+
+            while li <= ri:
+                m = li + (ri - li) // 2
+
+                if nums[m] == target:
+                    result = m
+                    li = m + 1
+                elif nums[m] < target:
+                    li = m + 1
+                else:
+                    ri = m - 1
+
+            return result
+
+        left = find_left()
+        if left == -1:
             return [-1, -1]
 
-        left_index = find_left_index(nums, any_index, target)
-        right_index = find_right_index(nums, any_index, target)
-
-        return [left_index, right_index]
+        return [left, find_right()]
