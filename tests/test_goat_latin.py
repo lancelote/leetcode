3681ee0fce3e28a3d6d61a0147f5e3b1dad5efa3
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-08-18 13:40:26 +0200

    use explicit parenthesis over string implicit concatenation

--- tests/test_goat_latin.py
+++ tests/test_goat_latin.py
@@ -9,8 +9,10 @@ from src.goat_latin import Solution
         ("I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"),
         (
             "The quick brown fox jumped over the lazy dog",
-            "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa "
-            "hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa",
+            (
+                "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa "
+                "overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+            ),
         ),
     ),
 )
