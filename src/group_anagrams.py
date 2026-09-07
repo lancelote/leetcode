b88dfbda7058d5135b9cf1c9bf82420e7bb36c30
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-09-07 21:04:40 +0200

    re-solve "49. Group Anagrams"

--- src/group_anagrams.py
+++ src/group_anagrams.py
@@ -1,22 +1,12 @@
 from collections import defaultdict
 
 
-def get_count(word: str) -> tuple[int, ...]:
-    counts = [0] * 26
-
-    for letter in word:
-        index = ord(letter) - ord("a")
-        counts[index] += 1
-
-    return tuple(counts)
-
-
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        count_to_words: dict[tuple[int, ...], list[str]] = defaultdict(list)
+        anagrams: dict[str, list[str]] = defaultdict(list)
 
         for word in strs:
-            count = get_count(word)
-            count_to_words[count].append(word)
+            key = "".join(sorted(word))
+            anagrams[key].append(word)
 
-        return list(count_to_words.values())
+        return list(anagrams.values())
