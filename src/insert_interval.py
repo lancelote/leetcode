21879634d92f7c786e846958baabceeeb5fd5b96
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-09-11 21:56:30 +0200

    solve "57. Insert Interval"

--- src/insert_interval.py
+++ src/insert_interval.py
@@ -3,27 +3,28 @@ class Solution:
         self, intervals: list[list[int]], new_interval: list[int]
     ) -> list[list[int]]:
         n = len(intervals)
-        result: list[list[int]] = []
 
+        result: list[list[int]] = []
         new_start, new_end = new_interval
 
         for i, interval in enumerate(intervals):
             start, end = interval
 
-            # interval before the new one
-            if end < new_start:
-                result.append(interval)
-
-            # interval after the new one
-            elif start > new_end:
+            # new is before
+            if new_end < start:
                 result.append([new_start, new_end])
-                result.extend(intervals[j] for j in range(i, n))
+                result.extend(intervals[i] for i in range(i, n))
                 return result
 
-            # collision
+            # new is after
+            elif new_start > end:
+                result.append(interval)
+
+            # new overlaps
             else:
                 new_start = min(new_start, start)
                 new_end = max(new_end, end)
 
         result.append([new_start, new_end])
+
         return result
