5abc76c323b1dd09a2ca997856450822f6c8c702
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-09-19 16:32:40 +0200

    re-solve "91. Decode Ways"

--- src/decode_ways.py
+++ src/decode_ways.py
@@ -1,15 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        cache = [0] * (len(s) + 1)
-        cache[len(s)] = 1
+        n = len(s)
+        cache: list[int] = [0] * (n + 1)
+        cache[n] = 1
 
-        for i in range(len(s) - 1, -1, -1):
+        for i in range(n - 1, -1, -1):
             if s[i] == "0":
                 cache[i] = 0
             else:
                 cache[i] = cache[i + 1]
 
-            if i + 1 < len(s) and int(s[i : i + 2]) <= 26 and s[i] != "0":
-                cache[i] += cache[i + 2]
+                if i < (n - 1) and int(s[i : i + 2]) <= 26:
+                    cache[i] += cache[i + 2]
 
         return cache[0]
