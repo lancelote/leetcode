7b3bbf71731751986d7438e8eedfabac655f22d2
Pavel Karateev <lancelote.du.lac@gmail.com>
2026-09-25 17:37:03 +0200

    drop unused variable

--- src/unique_paths.py
+++ src/unique_paths.py
@@ -2,7 +2,7 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [1] * n
 
-        for i in range(m - 2, -1, -1):
+        for _ in range(m - 2, -1, -1):
             new_dp = [0] * n
             new_dp[-1] = 1
 
